03e2a21395e36ba8afc7a20cd0fd46a2d0704313
mtgao2 <michael.gao1@gmail.com>
2018-02-11 00:06:32 -0600

    Add postgres deploy with persistant volume

---
+++ sql/create_tables.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS account (
+  user_name varchar(256) PRIMARY KEY,
+  user_password varchar(256)
+);
---
+++ sql/init.sql
@@ -0,0 +1,6 @@
+ALTER USER postgres PASSWORD 'postgres';
+CREATE ROLE readonly PASSWORD 'readonly';
+REVOKE CREATE ON SCHEMA public FROM public;
+GRANT CREATE ON SCHEMA public TO postgres;
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM readonly;
+
