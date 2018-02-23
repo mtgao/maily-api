72981a2ceb05465d081668f24d5b99e22d4555ea
mtgao2 <michael.gao1@gmail.com>
2018-02-22 21:48:49 -0600

    Setup graphql endpoint

--- server.js
+++ server.js
@@ -1,19 +1,54 @@
-const http = require('http');
 const db = require('./store/db');
-
-db.select().from('accounts').then(function(rows){
-    console.log(rows)
-});
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
 
 const hostname = '0.0.0.0';
 const port = 8000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { 
+    accounts: [Account] 
+  }
+  type Account { 
+    user_name: String, 
+    user_password: String 
+  }
+`;
+
+function findAccount(search) {
+  return db.select().from('accounts');
+}
+
+// The resolvers
+const resolvers = {
+  Query: { 
+    accounts: (_, {username}) => 
+      findAccount({ user_name: username }).then(
+        rows => rows
+      )
+  }
+};
+  
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// Initialize the app
+const app = express();
+
+// The GraphQL endpoint
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+// GraphiQL, a visual editor for queries
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+// Start the server
+app.listen(8000, () => {
+  console.log('Go to http://localhost:8000/graphiql to run queries!');
 });
