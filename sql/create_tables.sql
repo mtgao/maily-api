7453871a3d3085f142648bb081339f916734bdac
mtgao2 <michael.gao1@gmail.com>
2018-02-25 22:21:54 -0600

    Create celeb/media tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,3 +2,21 @@ CREATE TABLE IF NOT EXISTS accounts (
   user_name varchar(256) PRIMARY KEY,
   user_password varchar(256)
 );
+
+CREATE TABLE IF NOT EXISTS celebrity (
+  id serial PRIMARY KEY,
+  name varchar(128),
+  occupation varchar(32),
+  nationality varchar(32),
+  dob date,
+  appearances jsonb
+);
+
+CREATE TABLE IF NOT EXISTS media (
+  id serial PRIMARY KEY,
+  name varchar(128),
+  description varchar(128),
+  link varchar(256),
+  category varchar(32),
+  celebrities jsonb
+);
