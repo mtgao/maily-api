2fd87d795b6e73a650037fdd7aaf5da025840364
mtgao2 <michael.gao1@gmail.com>
2018-02-26 19:52:30 -0600

    Reduce name size and add enum for categories

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -5,18 +5,38 @@ CREATE TABLE IF NOT EXISTS accounts (
 
 CREATE TABLE IF NOT EXISTS celebrity (
   id serial PRIMARY KEY,
-  name varchar(128),
+  name varchar(64),
   occupation varchar(32),
   nationality varchar(32),
   dob date,
   appearances jsonb
 );
 
+
+/* Define category enum */ 
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'categories') THEN
+        CREATE TYPE categories as ENUM (
+          'interview',
+          'talk show',
+          'commercial',
+          'variety show',
+          'music show',
+          'television',
+          'movie',
+          'vlive',
+          'radio show',
+          'misc'
+        );
+    END IF;
+END$$;
+
 CREATE TABLE IF NOT EXISTS media (
   id serial PRIMARY KEY,
   name varchar(128),
   description varchar(128),
   link varchar(256),
-  category varchar(32),
+  category categories,
   celebrities jsonb
 );
